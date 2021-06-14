79bcd1221af4f85d3c0d3d6a099904c5ab2dfaf0
Arthur Mouradian <arthur@Arthurs-MBP.fios-router.home>
2021-06-13 20:29:33 -0400

    added some js for adding columns

--- script.js
+++ script.js
@@ -4,8 +4,36 @@ let colorSelected;
 
 //Adds a row
 function addR() {
-    alert("Clicked Add Row")
-    console.log("clicked add row")
+    var myTable = document.getElementById("grid");
+    var currentIndex = myTable.rows.length;
+    var currentRow = myTable.insertRow(-1);
+
+    var linksBox = document.createElement("input");
+    linksBox.setAttribute("name", "links" + currentIndex);
+
+    var keywordsBox = document.createElement("input");
+    keywordsBox.setAttribute("name", "keywords" + currentIndex);
+
+    var violationsBox = document.createElement("input");
+    violationsBox.setAttribute("name", "violationtype" + currentIndex);
+
+    var addRowBox = document.createElement("input");
+    addRowBox.setAttribute("type", "button");
+    addRowBox.setAttribute("value", "Add another line");
+    addRowBox.setAttribute("onclick", "addField();");
+    addRowBox.setAttribute("class", "button");
+
+    var currentCell = currentRow.insertCell(-1);
+    currentCell.appendChild(linksBox);
+
+    currentCell = currentRow.insertCell(-1);
+    currentCell.appendChild(keywordsBox);
+
+    currentCell = currentRow.insertCell(-1);
+    currentCell.appendChild(violationsBox);
+
+    currentCell = currentRow.insertCell(-1);
+    currentCell.appendChild(addRowBox);
 }
 //Adds a column
 function addC() {
